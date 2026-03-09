730b0a106f7929385b06dd683315c9dd764b805d
gokithasri-2023 <gokithasri.s2023@vitstudent.ac.in>
2026-03-09 22:19:22 +0530

    App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,38 @@
+package com.example;
 
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter marks for 5 subjects:");
+
+        int total = 0;
+
+        for(int i=1;i<=5;i++)
+        {
+            System.out.print("Subject " + i + ": ");
+            int marks = sc.nextInt();
+            total += marks;
+        }
+
+        double average = total / 5.0;
+
+        System.out.println("Total Marks: " + total);
+        System.out.println("Average: " + average);
+
+        if(average >= 90)
+            System.out.println("Grade: A");
+        else if(average >= 75)
+            System.out.println("Grade: B");
+        else if(average >= 60)
+            System.out.println("Grade: C");
+        else
+            System.out.println("Grade: D");
+
+        sc.close();
+    }
+}
